27dbb7e4b26e17764323f27644b919300ae6e874
Gmathivathanan <gmathivathanan4@gmail.com>
2023-03-19 10:52:19 +0530

    API Test completed

--- ExpertSystemTests/ExpertSystemTests.swift
+++ ExpertSystemTests/ExpertSystemTests.swift
@@ -18,6 +18,8 @@ final class ExpertSystemTests: XCTestCase {
         
     }
     
+    // MARK: API Test - GET Scenarios
+    
     func testGetScenarios() {
         let scenarioSample = Scenario(caseId: 1, text: "Power button will not start computer")
         
@@ -27,6 +29,28 @@ final class ExpertSystemTests: XCTestCase {
             switch result {
             case .success(let data):
                 XCTAssertEqual(data[0].text, scenarioSample.text)
+                expectation.fulfill()
+                break
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+        waitForExpectations(timeout: 5)
+    }
+    
+    // MARK: API Test - GET Cases
+    
+    func testGetCase() {
+        let caseSample = Case(id: 1, text: "check the power cord to confirm that it is plugged securely into the back of the computer")
+        
+        let expectation = self.expectation(description: "Case_API")
+        
+        CaseAPI.getCaseData(of: 1) { result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                XCTAssertEqual(data[0].text, caseSample.text)
                 expectation.fulfill()
                 break
             case .failure(let error):
